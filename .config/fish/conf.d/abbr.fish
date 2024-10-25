f798d1aa9af62dbf6100b83ad85000ed5aac712a
Simon Rondelez <simonrondelez@me.com>
2024-10-25 16:42:51 +0200

    sync

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,18 +1,18 @@
 # ls
-abbr l 'exa -lgh'
-abbr la 'exa -lgha'
+abbr l 'eza -lgh'
+abbr la 'eza -lgha'
 
 # shortcuts
 abbr k kubectl
 abbr kube-dashboard 'kubectl auth-proxy -n kubernetes-dashboard https://kubernetes-dashboard.svc'
 abbr fly-clean 'fly prune-worker --all-stalled'
 abbr tfdocs terraform-docs
-
+abbr sso-login 'aws sso login --sso-session SkyscrapersSSO'
 # I want unset
 abbr unset 'set -e'
 
 # Git
-abbr cdroot 'cd $(git root)'
+abbr cdroot 'cd $(git rev-parse --show-toplevel)'
 abbr gitlog "git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
 abbr gitclean "git branch --merged | grep -v 'master' | grep -v 'main' | xargs git branch -D"
 abbr gitclean-recursively 'find . -maxdepth 3 -type d -exec sh -c "cd {}; git branch --merged | grep -v 'master' | grep -v 'main' | xargs git branch -D" \;'
